128e266f9a8257317040e6c2d84a2284ccc862b8
Aswin G <aswinganesh666@gmail.com>
2019-02-11 22:47:25 +0530

    Created basic structure and added router

--- src/App.js
+++ src/App.js
@@ -1,28 +1,20 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import DalalBull from "./pages/DalalBull";
+import Kryptos from "./pages/Kryptos";
+import Login from "./pages/Login";
+import "./App.css";
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
-}
+const App = () => (
+  <Router>
+    <div>
+      <Route exact path="/" component={Home} />
+      <Route path="/login" component={Login} />
+      <Route path="/dalalbull" component={DalalBull} />
+      <Route path="/kryptos" component={Kryptos} />
+    </div>
+  </Router>
+);
 
 export default App;
---
+++ src/pages/Home.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "../App.css";
+
+class Home extends Component {
+  render() {
+    return <div>HOMEPAGE!!</div>;
+  }
+}
+
+export default Home;
