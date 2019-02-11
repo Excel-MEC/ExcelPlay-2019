5168f3641ec1ea846a4860e6d92e9cbb94cacc3b
Aswin G <aswinganesh666@gmail.com>
2019-02-11 22:47:25 +0530

    Created basic structure and added router

---
+++ src/pages/DalalBull.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "../App.css";
+
+class DalalBull extends Component {
+  render() {
+    return <div>DALALBULL!!</div>;
+  }
+}
+
+export default DalalBull;
