b2d1d22ca298224628091f1260b9b9df5900f46f
Ajesh Kumar S <ajeshkumar36020@gmail.com>
2019-02-13 12:27:08 +0530

    Added esilint, prettier,.. by @amp

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,8 @@
-import React, { Component } from "react";
-import "../App.css";
+import React from 'react';
+import '../App.css';
 
-class Home extends Component {
-  render() {
-    return <div>HOMEPAGE!!</div>;
-  }
-}
+const Home = () => {
+  return <div>home</div>;
+};
 
 export default Home;
