110329c0798b507b7a8095f115f3882b00fd04d0
Ajesh Kumar S <ajeshkumar36020@gmail.com>
2019-02-20 09:42:51 +0530

    Started with navbar

---
+++ src/components/common/Navbar/Navbar.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <ul className="nav navbar-nav my-auto">
+      <li>
+        <a href="/" className="active">
+          <i className="fa fa-home" />
+        </a>
+      </li>
+      <li>
+        <a href="/">
+          <i className="fa fa-suitcase" />
+        </a>
+      </li>
+      <li>
+        <a href="/">
+          <i className="fa fa-shopping-cart" />
+        </a>
+      </li>
+      <li>
+        <a href="/">
+          <i className="fa fa-book" />
+        </a>
+      </li>
+    </ul>
+  );
+};
+
+export default Navbar;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'popper.js/dist/popper.min';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min';
+import 'font-awesome/css/font-awesome.min.css';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
