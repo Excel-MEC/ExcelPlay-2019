a0c12eb114e0b9577cfabdc5a64331848ae59ac8
Aswin M Prabhu <aswinmprabhu@gmail.com>
2019-02-21 23:33:44 +0530

    Add buy,short buttons and quantity input

--- src/components/Dalalbull/DalalbullPlay/ShareDetails/ShareDetails.js
+++ src/components/Dalalbull/DalalbullPlay/ShareDetails/ShareDetails.js
@@ -1,7 +1,26 @@
 import React from 'react';
+import './ShareDetails.css';
 
 const ShareDetails = () => {
-  return <div />;
+  return (
+    <div>
+      <div className="quantity-wrapper">
+        <input
+          type="text"
+          name="quantity"
+          id="quantity-input"
+          className="quantity-input"
+          placeholder="Quantity"
+        />
+      </div>
+      <button type="button" className="buy-button">
+        BUY
+      </button>
+      <button type="button" className="short-button">
+        SHORT
+      </button>
+    </div>
+  );
 };
 
 export default ShareDetails;
