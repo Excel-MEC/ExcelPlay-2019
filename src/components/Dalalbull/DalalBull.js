bfb3f36629a47deab2b81d76b94172254beb8146
Ajesh Kumar S <ajeshkumar36020@gmail.com>
2019-02-22 14:54:40 +0530

    Dalalbull routes and routes added

--- src/components/Dalalbull/DalalBull.js
+++ src/components/Dalalbull/DalalBull.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react';
+import {Route} from 'react-router-dom';
 import DalalbullHeader from './DalalbullHeader/DalalbullHeader';
 import DalalbullPlay from './DalalbullPlay/DalalbullPlay';
-import './DalalBull.css';
+import DalalbullRules from './DalalbullRules/DalalbullRules';
 
 class DalalBull extends Component {
   render() {
+    const { match } = this.props;
     return (
       <div>
         <DalalbullHeader />
-        <DalalbullPlay />
+        <Route exact path={`${match.url}/`} component={DalalbullPlay}/>
+        <Route path={`${match.url}/rules`} component={DalalbullRules}/>
       </div>
     );
   }
--- src/components/Dalalbull/DalalbullPlay/ShareDetails/ShareDetails.js
+++ src/components/Dalalbull/DalalbullPlay/ShareDetails/ShareDetails.js
@@ -1,22 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ShareDetails.css';
 
 const ShareDetails = () => {
+  const [quantity, setQuantity] = useState(0);
   return (
     <div>
       <div className="quantity-wrapper">
+        <button
+          className="btn btn-danger increment-button"
+          onClick={e => setQuantity(quantity - 1)}
+        >
+          <icon className="fa fa-minus" />
+        </button>
         <input
-          type="text"
+          type="number"
           name="quantity"
           id="quantity-input"
           className="quantity-input"
           placeholder="Quantity"
+          value={quantity}
+          onChange={e => setQuantity(e.target.value)}
         />
+        <button
+          className="btn btn-success increment-button"
+          onClick={e => setQuantity(quantity + 1)}
+        >
+          <icon className="fa fa-plus" />
+        </button>
       </div>
-      <button type="button" className="buy-button">
+      <button type="button" className="btn btn-success">
         BUY
       </button>
-      <button type="button" className="short-button">
+      <button type="button" className="btn btn-danger">
         SHORT
       </button>
     </div>
