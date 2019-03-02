ef53135da9551a94b44ce6ab7bdb2b18e65b38e8
Aswin G <aswinganesh666@gmail.com>
2019-03-02 23:04:15 +0530

    Restructure home

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
-import Home from './components/Home';
+import Home from './components/Home/Home';
 import './App.scss';
 
 const DalalBull = lazy(() => import('./components/Dalalbull/DalalBull'));
