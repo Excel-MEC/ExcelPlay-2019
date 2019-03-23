2fcd9ad7e0d81b7a915ecc061a1ee1dfb5f00f50
JoyalAJohney <joyalajohny@gmail.com>
2019-03-23 13:07:47 +0530

    Logos added to HomePage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,22 +1,36 @@
 import React from 'react';
 import HomeHeader from '../common/HomeHeader/HomeHeader';
 import './home.scss';
-import Navbar from '../common/Navbar/Navbar';
+// import Navbar from '../common/Navbar/Navbar';
+import dalal from '../../assets/bull5-grey.png';
+import kryp from '../../assets/kry-grey.png';
+import hash from '../../assets/hash2-grey.png';
+import echo from '../../assets/ech2-grey.png';
 
 const Home = () => {
   return (
     <div>
-      <Navbar />
+      {/* <Navbar /> */}
       <HomeHeader />
       <div className="events">
         <div className="row">
           <div className="col-md-6 center-block text-center cell">
+            <img src={dalal} alt="dalabull" className="dalalPic" />
             DALAL BULL
           </div>
-          <div className="col-md-6 center-block text-center cell">KRYTPOS</div>
+          <div className="col-md-6 center-block text-center cell">
+            <img src={kryp} alt="kryptos" className="krypPic" />
+            KRYTPOS
+          </div>
           <div className="w-100" />
-          <div className="col-md-6 center-block text-center cell">#INCLUDE</div>
-          <div className="col-md-6 center-block text-center cell">ECHO</div>
+          <div className="col-md-6 center-block text-center cell">
+            <img src={hash} alt="#include" className="hashPic" />
+            #INCLUDE
+          </div>
+          <div className="col-md-6 center-block text-center cell">
+            <img src={echo} alt="echo" className="echoPic" />
+            ECHO
+          </div>
         </div>
       </div>
     </div>
