30ca3d82547bd43cd1bdc5e546cb1f83ea2a9f93
Aswin G <aswinganesh666@gmail.com>
2019-03-23 13:10:30 +0530

    Update Kryptos.js to use KryptosInfoBar

--- src/components/Kryptos/Kryptos.js
+++ src/components/Kryptos/Kryptos.js
@@ -1,8 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import { GameHeader, MItem } from '../common/GameHeader/GameHeader';
+import kryptosLogo from '../../assets/kry-grey.png';
 import '../../App.scss';
+import KryptosInfoBar from './KryptosInfoBar/KryptosInfoBar';
 
 const Kryptos = () => {
-  return <div>Kryptos</div>;
+  const [level, setLevel] = useState(1);
+  const [rank, setRank] = useState(0);
+
+  useEffect(() => {
+    // TODO: Make a request to API to fetch level and rank
+    setLevel(1);
+    setRank(0);
+  });
+
+  return (
+    <div>
+      <GameHeader gName="KRYPTOS" icon={kryptosLogo}>
+        <MItem text="Rules" link="/Kryptos/rules" />
+        <MItem text="Play" link="/Kryptos" />
+        <MItem text="Ranklist" link="/Kryptos/ranklist" />
+      </GameHeader>
+      <KryptosInfoBar level={level} rank={rank} />
+    </div>
+  );
 };
 
 export default Kryptos;
