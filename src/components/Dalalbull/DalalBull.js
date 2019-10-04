b3e8675fe08c7601459782dd5a4f5212db2c8e32
Ajesh Kumar S <ajeshkumar36020@gmail.com>
2019-10-04 13:24:52 +0530

    Changed docker configuration and working on Portfolio

--- src/components/Dalalbull/DalalBull.js
+++ src/components/Dalalbull/DalalBull.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import DalalbullHeader from './DalalbullComponents/DalalbullHeader/DalalbullHeader';
+import DalalbullPortfolio from './DalalbullPortfolio/DalalbullPortfolio';
 import DalalbullPlay from './DalalbullPlay/DalalbullPlay';
 import DalalbullRules from './DalalbullRules/DalalbullRules';
 import DalalbullRanklist from './DalalbullRanklist/DalalbullRanklist';
@@ -14,7 +15,8 @@ const Dalalbull = props => {
   return (
     <div>
       <DalalbullHeader />
-      <Route exact path={`${match.url}/`} component={DalalbullPlay} />
+      <Route exact path={`${match.url}/`} component={DalalbullPortfolio} />
+      <Route path={`${match.url}/:cid`} component={DalalbullPlay} />
       <Route path={`${match.url}/rules`} component={DalalbullRules} />
       <Route path={`${match.url}/ranklist`} component={DalalbullRanklist} />
     </div>
