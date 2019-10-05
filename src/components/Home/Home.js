c3fed59718a8328ff75536ffb81b4fb0372cb215
agzuniverse <aswinganesh666@gmail.com>
2019-10-06 00:43:22 +0530

    Display correct ranks for Kryptos and Dalalbull at homepage

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,7 +1,8 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import HomeHeader from '../common/HomeHeader/HomeHeader';
 import Games from './Games/Games';
 import './Home.scss';
+import { ApiRoot } from '../../config/api';
 // import Navbar from '../common/Navbar/Navbar';
 import dalal from '../../assets/bull5-grey.png';
 import kryp from '../../assets/kry-grey.png';
@@ -9,6 +10,27 @@ import hash from '../../assets/hash2-grey.png';
 import echo from '../../assets/ech2-grey.png';
 
 const Home = () => {
+  const [kryptosRank, setKryptosRank] = useState(0);
+  const [dalalbullRank, setDalalbullRank] = useState(0);
+  useEffect(() => {
+    fetch(`${ApiRoot}/auth/leaderboard/rank`, {
+      mode: 'cors',
+    })
+      .then(res => {
+        return res.json();
+      })
+      .then(data => {
+        if (data.kryptos != null)
+          setKryptosRank(data.kryptos.rank);
+        else
+          setKryptosRank(0);
+        if (data.dalalbull != null)
+          setDalalbullRank(data.dalalbull.rank);
+        else
+          setDalalbullRank(0);
+      });
+  }, [])
+
   return (
     <div className="row">
       <div className="col-md-12">
@@ -20,7 +42,7 @@ const Home = () => {
             <div className="col-md-6 center-block text-center cell">
               <Games
                 name="DALALBULL"
-                rank="12"
+                rank={dalalbullRank}
                 logo={dalal}
                 imageSize="dalalPic"
                 type="ranked"
@@ -30,7 +52,7 @@ const Home = () => {
             <div className="col-md-6 center-block text-center cell">
               <Games
                 name="KRYPTOS"
-                rank="21"
+                rank={kryptosRank}
                 logo={kryp}
                 imageSize="krypPic"
                 type="ranked"
