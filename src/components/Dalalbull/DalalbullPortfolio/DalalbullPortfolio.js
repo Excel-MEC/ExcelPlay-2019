4c377c43de5893543dc0c3103222594de68cda62
Ajesh Kumar S <ajeshkumar36020@gmail.com>
2019-10-06 13:04:37 +0530

    Removed History and added Portfolio in the menu with exclusive Portfolio view Page

--- src/components/Dalalbull/DalalbullPortfolio/DalalbullPortfolio.js
+++ src/components/Dalalbull/DalalbullPortfolio/DalalbullPortfolio.js
@@ -7,7 +7,7 @@ import ShareDetails from '../DalalbullComponents/ShareDetails/ShareDetails';
 import GraphAndStatus from '../DalalbullComponents/GraphAndStatus/GraphAndStatus';
 import './DalalbullPortfolio.scss';
 
-const DalalbullPortfolio = () => {
+const HomeView = () => {
   return (
     <div className="dalalbullPortfolio">
       <Ticker />
@@ -20,7 +20,7 @@ const DalalbullPortfolio = () => {
           </div>
           <div className="col-md-5 d-none d-sm-block history">
             <h2 className="h2">Portfolio</h2>
-            <Portfolio/>
+            <Portfolio />
           </div>
           <div id="share-modal" className="modal fade" role="dialog">
             <div className="modal-dialog">
@@ -36,6 +36,30 @@ const DalalbullPortfolio = () => {
       </div>
     </div>
   );
+};
+
+const ExclusiveView = () => {
+  return (
+    <div className="dalalbullPortfolio">
+      <Ticker />
+      <div className="content" style={{ padding: '1em' }}>
+        <div className="row">
+          <div className="col-md-6 d-sm-block history">
+            <h2 className="h2">Portfolio</h2>
+            <Portfolio />
+          </div>
+          <div className="col-md-6">
+            <GraphAndStatus />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const DalalbullPortfolio = props => {
+  if (props.match.url.includes('Portfolio')) return <ExclusiveView />;
+  else return <HomeView />;
 };
 
 export default DalalbullPortfolio;
