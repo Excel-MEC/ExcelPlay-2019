8689f87ef95242c33d5472740cb7e7e6d2826afd
agzuniverse <aswinganesh666@gmail.com>
2019-10-07 00:49:03 +0530

    Add leaderboard for Kryptos

--- src/components/Kryptos/KryptosRanklist/KryptosRanklist.js
+++ src/components/Kryptos/KryptosRanklist/KryptosRanklist.js
@@ -1,25 +1,51 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './KryptosRanklist.scss';
+import { ApiRoot } from '../../../config/api';
 
 const KryptosRanklist = () => {
+    const [ranklist, setRanklist] = useState([]);
+
+    useEffect(() => {
+        fetch(`${ApiRoot}/auth/leaderboard/kranklist`, {
+            mode: 'cors'
+        }).then(res => {
+            return res.json();
+        }).then(data => {
+            setRanklist(data.ranklist);
+        })
+    }, []);
+
+    const RankListRows = ({ ranklist }) =>
+        (
+            <>
+                {
+                    ranklist.map(curr => (
+                        <tr>
+                            <td><img src={curr.pic} alt="" class="propic" /></td>
+                            <td>{curr.name}</td>
+                            <td>{curr.rank}</td>
+                            <td>{curr.level}</td>
+                        </tr>
+                    ))
+                }
+            </>
+        );
+
+
     return (
         <div className="content">
             <h2>Ranklist</h2>
             <table className="table">
-                <tr>
-                    <th>#</th>
-                    <th>Name</th>
-                    <th>Net Worth</th>
-                </tr>
-                <tbody>
+                <thead>
                     <tr>
-                        <td>1</td>
-                        <td>
-                            {/* <img src="{{ record.pic }}" alt="" class="propic"> */}
-                            Name
-            </td>
-                        <td>55</td>
+                        <th></th>
+                        <th>Name</th>
+                        <th>Rank</th>
+                        <th>Level</th>
                     </tr>
+                </thead>
+                <tbody>
+                    <RankListRows ranklist={ranklist} />
                 </tbody>
             </table>
         </div>
