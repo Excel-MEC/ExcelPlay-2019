c659bbb98c25e4697f041fe6ea1769ace1878a68
agzuniverse <aswinganesh666@gmail.com>
2019-10-10 21:17:57 +0530

    Ran prettier

--- src/components/Kryptos/KryptosApi/ApiCalls.js
+++ src/components/Kryptos/KryptosApi/ApiCalls.js
@@ -1,22 +1,22 @@
-import { ApiRoot } from "../../../config/api";
-import * as http from "../../../config/http";
+import { ApiRoot } from '../../../config/api';
+import * as http from '../../../config/http';
 
 export const fetchQuestion = () => {
-    return http.get(`${ApiRoot}/kryptos/api/ask`);
+  return http.get(`${ApiRoot}/kryptos/api/ask`);
 };
 
 export const fetchRank = () => {
-    return http.get(`${ApiRoot}/auth/leaderboard/rank`);
+  return http.get(`${ApiRoot}/auth/leaderboard/rank`);
 };
 
-export const submitKryptosAnswer = (ans) => {
-    return http.get(`${ApiRoot}/kryptos/api/answer?answer=${ans}`);
+export const submitKryptosAnswer = ans => {
+  return http.get(`${ApiRoot}/kryptos/api/answer?answer=${ans}`);
 };
 
 export const fetchKryptosLeaderboard = () => {
-    return http.get(`${ApiRoot}/auth/leaderboard/kranklist`);
+  return http.get(`${ApiRoot}/auth/leaderboard/kranklist`);
 };
 
-export const getStaticAsset = (filename) => {
-    return `${ApiRoot}${filename}`;
+export const getStaticAsset = filename => {
+  return `${ApiRoot}${filename}`;
 };
