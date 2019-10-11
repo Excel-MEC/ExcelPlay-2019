71ba414dbf277776aa83b9fd954eef4f666799e7
Ajesh Kumar S <ajeshkumar36020@gmail.com>
2019-10-11 12:01:48 +0530

    minor change

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { lazy, Suspense, useState } from 'react';
+import React, { lazy, Suspense } from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Home from './components/Home/Home';
 import Spinner from './components/common/Spinner/Spinner';
